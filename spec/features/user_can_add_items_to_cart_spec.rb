726cda692fdd44b6a82bc1b041f25350368e59b5
Jennifer <jennifersoden@hotmail.com>
2016-06-09 23:12:37 -0600

    delete comments

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -19,14 +19,5 @@ RSpec.feature "User can add an item to their cart" do
     expect(page).to have_content("#{item.weight}")
     expect(page).to have_content("Price Total: #{item.price}")
     expect(page).to have_content("Weight Total: #{item.weight}")
-
-# As a visitor
-# When I visit any page with an item on it
-# I should see a link or button for "Add to Cart"
-# When I click "Add to cart" for that item
-# And I click a link or button to view cart
-# And my current path should be "/cart"
-# And I should see a small image, title, description and price for the item I just added
-# And there should be a "total" price for the cart that should be the sum of all items in the cart
   end
 end
